89f27eb22826ed6964dd6cd11f63b726278f096d
Jacher <jwc2002@outlook.com>
2022-01-27 10:31:35 +0000

    change jobresult scheme

--- src/job.rs
+++ src/job.rs
@@ -2,15 +2,30 @@ use crate::errors::ProcessingError;
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub enum JobResult {
-    Image((usize, Vec<u8>)),
-    Error((usize, ProcessingError))
+pub struct JobResult {
+    id: usize,
+    pub result: Result<Vec<u8>, ProcessingError>
 }
 impl JobResult {
-    pub fn id(&self) -> usize {
-        match self {
-            JobResult::Image(id) => id.0,
-            JobResult::Error(id) => id.0
+    pub fn new(id: usize, result: Result<Vec<u8>, ProcessingError>) -> Self {
+        Self {
+            id,
+            result
         }
+    }
+    pub fn new_ok(id: usize, result: Vec<u8>) -> Self {
+        Self {
+            id,
+            result: Ok(result)
+        }
+    }
+    pub fn new_err(id: usize, result: ProcessingError) -> Self {
+        Self {
+            id,
+            result: Err(result)
+        }
+    }
+    pub fn id(&self) -> usize {
+        self.id
     }
 }
